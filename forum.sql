6366e56d408c251fafa4621eaefcf2b28453dc05
johanodell <35562269+johanodell@users.noreply.github.com>
2020-09-10 11:20:00 +0200

    Update forum.sql

--- forum.sql
+++ forum.sql
@@ -1,4 +1,4 @@
-mysql -h localhost -u root -p
+mysql -h localhost -u root -pml7MsBkvQ3AbUoSH
 mysql> use default
 mysql> create table redhat (platform varchar(20));
 mysql> insert into redhat values ('virtual machine');
