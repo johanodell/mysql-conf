a22391425bad6c050825d169e7144865d143f1fb
johanodell <35562269+johanodell@users.noreply.github.com>
2020-09-10 11:25:49 +0200

    Update forum.sql

--- forum.sql
+++ forum.sql
@@ -1,4 +1,4 @@
-mysql -h localhost -u root -pml7MsBkvQ3AbUoSH
+mysql -h localhost -u cakephp -pml7MsBkvQ3AbUoSH
 mysql> use default
 mysql> create table redhat (platform varchar(20));
 mysql> insert into redhat values ('virtual machine');
